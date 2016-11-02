48c5930e73c38e5ee0c5654a38b7cf99929000d9
Eugen Istoc <eugenistoc@gmail.com>
2016-11-01 21:12:38 -0400

    feat(Aded sidebar component): add some features

--- src/components/sidebar/index.js
+++ src/components/sidebar/index.js
@@ -1,2 +1,3 @@
 module.exports = angular.module('angular-lte.sidebar', [])
+    .component('lteSidebar', require('./sidebar.component'))
     .service('AngularLTESidebar', require('./sidebar.service'));
---
+++ src/components/sidebar/sidebar.component.js
@@ -0,0 +1,9 @@
+module.exports = {
+    template: `
+        <aside class="main-siderbar">
+            <section class="sidebar" style="height: auto;">
+                
+            </section>
+        </aside>
+    `
+}
